f74a2c1b2f4266b7b520beda32c6ca30716f9c1c
Danny van Kooten <dannyvankooten@gmail.com>
2015-02-04 21:51:25 +0700

    Fix issue where bar overlays menu or other elements. Fixes #18 .

--- assets/js/bar.js
+++ assets/js/bar.js
@@ -1,5 +1,7 @@
 (function($) {
 
+	var $body = $("body");
+
 	/**
 	 * Creates a new Top Bar from an element
 	 *
@@ -22,13 +24,24 @@
 		 *
 		 * @returns {boolean}
 		 */
-		function show() {
+		function show( manual ) {
 			if( $bar.is( ':animated' ) || visible ) {
 				return false;
 			}
 
-			$bar.slideDown();
-			eraseCookie( 'mctb_bar_hidden' );
+			if( manual ) {
+				// Add bar height to <body> padding
+				$body.animate( {
+					'padding-top': parseFloat( $("body").css('padding-top') ) + $bar.outerHeight()
+				} );
+				$bar.slideDown();
+				eraseCookie( 'mctb_bar_hidden' );
+			} else {
+				// Add bar height to <body> padding
+				$body.css( 'padding-top', parseFloat( $("body").css('padding-top') ) + $bar.outerHeight() );
+				$bar.show();
+			}
+
 			$icon.html(config.icons.hide);
 			visible = true;
 
@@ -40,14 +53,21 @@
 		 *
 		 * @returns {boolean}
 		 */
-		function hide() {
+		function hide(manual) {
 			if( $bar.is( ':animated' ) || ! visible ) {
 				return false;
 			}
 
-			$bar.slideUp();
+			if( manual ) {
+				$bar.slideUp();
+				$body.animate({ 'padding-top': 0 });
+				createCookie( "mctb_bar_hidden", 1, config.cookieLength );
+			} else {
+				$bar.hide();
+				$body.css('padding-top', 0);
+			}
+
 			visible = false;
-			createCookie( "mctb_bar_hidden", 1, config.cookieLength );
 			$icon.html(config.icons.show);
 
 			return true;
@@ -59,23 +79,19 @@
 		 * @returns {boolean}
 		 */
 		function toggle() {
-			return visible ? hide() : show();
+			return visible ? hide(true) : show(true);
 		}
 
 		// Code to run upon object instantiation
 
-		// Move element to begin of <body>
-		$wrapper.insertBefore( document.body.firstChild );
+		// Show the bar straight away?
+		if( readCookie( "mctb_bar_hidden" ) != 1 ) {
+			show()
+		}
+
 
 		// Listen to `click` events on the icon
 		$icon.click( toggle );
-
-		// Show the bar straight away?
-		if( readCookie( "mctb_bar_hidden" ) != 1 ) {
-			$bar.show();
-			visible = true;
-			$icon.html( config.icons.hide );
-		}
 
 		// Return values
 		return {
