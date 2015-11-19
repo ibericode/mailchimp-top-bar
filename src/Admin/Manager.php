e4c491c24ad714b610b6af3faf4547831e73bd45
Danny van Kooten <dannyvankooten@gmail.com>
2015-11-19 16:09:01 +0700

    Use `$current_tab` instead of `$tab` to store current tab.

--- src/Admin/Manager.php
+++ src/Admin/Manager.php
@@ -168,7 +168,7 @@ class Manager {
 	 */
 	public function show_settings_page() {
 
-		$tab = ( isset( $_GET['tab'] ) ) ? $_GET['tab'] : 'settings';
+		$current_tab = ( isset( $_GET['tab'] ) ) ? $_GET['tab'] : 'settings';
 		$opts = $this->options;
 		$lists = $this->get_mailchimp_lists();
 
