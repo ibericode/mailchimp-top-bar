f01bee77a6b514d9b28951ef7a37b500ab928f55
Danny van Kooten <dannyvankooten@gmail.com>
2015-11-25 13:11:37 +0700

    Bump dependency to MailChimp for WP v3.0 and improve admin notice when dependencies are not met.

--- dependencies.php
+++ dependencies.php
@@ -1,25 +1,23 @@
 <?php
 
-// check for mailchimp for wordpress pro 2.x
-if( defined( 'MC4WP_VERSION' ) && version_compare( MC4WP_VERSION, '2.5.6', '>=' ) ) {
-	return true;
-}
-
-// check for mailchimp for wordpress lite 2.x
-if( defined( 'MC4WP_LITE_VERSION' ) && version_compare( MC4WP_LITE_VERSION, '2.2.4', '>=' ) ) {
-	return true;
-}
-
-// check for MailChimp for WordPress core 3.x
+// check for MailChimp for WordPress (version 3.0 or higher)
 if( defined( 'MC4WP_VERSION' ) && version_compare( MC4WP_VERSION, '3.0', '>=' ) ) {
 	return true;
 }
 
 // Show notice to user
 add_action( 'admin_notices',  function() {
+
+	// only show to user with caps
+	if( ! current_user_can( 'install_plugins' ) ) {
+		return;
+	}
+
+	add_thickbox();
+	$url = network_admin_url( 'plugin-install.php?tab=plugin-information&plugin=mailchimp-for-wp&TB_iframe=true&width=600&height=550' );
 	?>
-	<div class="updated">
-		<p><?php printf( __( 'Please install or update <a href="%s">%s</a> in order to use %s.', 'mailchimp-top-bar' ), 'https://wordpress.org/plugins/mailchimp-for-wp/', 'MailChimp for WordPress', 'MailChimp Top Bar' ); ?></p>
+	<div class="notice notice-warning is-dismissible">
+		<p><?php printf( __( 'Please install or update <a href="%s" class="thickbox">%s</a> in order to use %s.', 'mailchimp-top-bar' ), $url, '<strong>MailChimp for WordPress</strong> (version 3.0 or higher)', 'MailChimp Top Bar' ); ?></p>
 	</div>
 <?php
 } );
