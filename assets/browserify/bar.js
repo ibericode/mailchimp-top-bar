cfa2485c2cb9b1b15680ac8d12147cfb0d3d576d
Danny van Kooten <dannyvankooten@gmail.com>
2016-08-25 11:39:51 +0200

    Recalculate body padding & other dimensions on window.resize (for better responsiveness).

--- assets/browserify/bar.js
+++ assets/browserify/bar.js
@@ -3,6 +3,29 @@
 var cookies = require('./cookies.js');
 var animator = require('./animator.js');
 
+function throttle(fn, threshhold, scope) {
+    threshhold || (threshhold = 250);
+    var last,
+        deferTimer;
+    return function () {
+        var context = scope || this;
+
+        var now = +new Date,
+            args = arguments;
+        if (last && now < last + threshhold) {
+            // hold on to it
+            clearTimeout(deferTimer);
+            deferTimer = setTimeout(function () {
+                last = now;
+                fn.apply(context, args);
+            }, threshhold);
+        } else {
+            last = now;
+            fn.apply(context, args);
+        }
+    };
+}
+
 function Bar( wrapperEl, config ) {
 
     // Vars & State
@@ -17,20 +40,11 @@ function Bar( wrapperEl, config ) {
 
 
     // Functions
-
     function init() {
-
-
 
         // remove "no_js" field
         var noJsField = barEl.querySelector('input[name="_mctb_no_js"]');
         noJsField.parentElement.removeChild(noJsField);
-
-        // calculate real bar height
-        var origBarPosition = barEl.style.position;
-        barEl.style.display = 'block';
-        barEl.style.position = 'relative';
-        barHeight = barEl.clientHeight;
 
         // save original bodyPadding
         if( isBottomBar ) {
@@ -40,12 +54,6 @@ function Bar( wrapperEl, config ) {
             wrapperEl.insertBefore( iconEl, barEl.nextElementSibling );
             originalBodyPadding = ( parseInt( document.body.style.paddingTop )  || 0 );
         }
-
-        // get real bar height (if it were shown)
-        bodyPadding = ( originalBodyPadding + barHeight ) + "px";
-
-        // fade response 4 seconds after showing bar
-        window.setTimeout(fadeResponse, 4000);
 
         // fix response height
         if( responseEl ) {
@@ -57,11 +65,39 @@ function Bar( wrapperEl, config ) {
         iconEl.innerHTML = config.icons.show;
         iconEl.addEventListener('click', toggle);
 
+        calculateDimensions();
+
+        // Show the bar straight away?
+        if( cookies.read( "mctb_bar_hidden" ) != 1 ) {
+            show()
+        }
+
+        // fade response 4 seconds after showing bar
+        window.setTimeout(fadeResponse, 4000);
+        window.addEventListener('resize', throttle(calculateDimensions));
+    }
+
+    function calculateDimensions() {
+
+        // make sure bar is visible
+        var origBarPosition = barEl.style.position;
+        var origBarDisplay = barEl.style.display;
+        barEl.style.display = 'block';
+        barEl.style.position = 'relative';
+
+        // calculate real bar height
+        barHeight = barEl.clientHeight;
+
+        // get real bar height (if it were shown)
+        bodyPadding = ( originalBodyPadding + barHeight ) + "px";
+
         // would the close icon fit inside the bar?
         var elementsWidth = 0;
         for( var i=0; i<barEl.firstElementChild.children.length; i++ ) {
             elementsWidth+= barEl.firstElementChild.children[i].clientWidth;
         }
+
+        wrapperEl.className = wrapperEl.className.replace('mctb-icon-inside-bar', '');
         if( elementsWidth + iconEl.clientWidth + 200 < barEl.clientWidth ) {
             wrapperEl.className += ' mctb-icon-inside-bar';
 
@@ -71,14 +107,9 @@ function Bar( wrapperEl, config ) {
             }
         }
 
-        // hide bar again, we're done measuring
-        barEl.style.display = 'none';
+        // reset bar again, we're done measuring
+        barEl.style.display = origBarDisplay;
         barEl.style.position = origBarPosition;
-
-        // Show the bar straight away?
-        if( cookies.read( "mctb_bar_hidden" ) != 1 ) {
-            show()
-        }
     }
 
     /**
