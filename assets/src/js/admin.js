ef5db3b0c48ab78285bb365a917700853eb887d7
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-01-05 12:21:14 +0100

    perform ajax request using plain XmlHttpRequest object instead of jQuery

--- assets/src/js/admin.js
+++ assets/src/js/admin.js
@@ -1,8 +1,5 @@
-const $ = window.jQuery
-const $context = $(document.getElementById('mc4wp-admin'))
 const elSelectList = document.getElementById('select-mailchimp-list')
 const msgRequiresFields = document.getElementById('message-list-requires-fields')
-const ajaxurl = window.ajaxurl
 
 /*
  * Functions
@@ -10,28 +7,35 @@ const ajaxurl = window.ajaxurl
 function maybeShowRequiredFieldsNotice () {
   msgRequiresFields.style.display = 'none'
   const listId = elSelectList.value
-  $.get(ajaxurl + '?action=mc4wp_get_list_details&ids=' + listId)
-    .then(lists => {
-      // iterate over selected lists
-      for (let i = 0; i < lists.length; i++) {
-        const list = lists[i]
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', window.ajaxurl + '?action=mc4wp_get_list_details&ids=' + listId, true);
+  xhr.onload = function() {
+    if (this.status >= 400) {
+      console.error("Error retrieving list details");
+      return;
+    }
+    const lists = JSON.parse(this.responseText);
+    // iterate over selected lists
+    for (let i = 0; i < lists.length; i++) {
+      const list = lists[i]
 
-        // iterate over list fields
-        for (let j = 0; j < list.merge_fields.length; j++) {
-          const field = list.merge_fields[j]
+      // iterate over list fields
+      for (let j = 0; j < list.merge_fields.length; j++) {
+        const field = list.merge_fields[j]
 
-          // if field other than EMAIL is required, show notice and stop loop
-          if (field.tag !== 'EMAIL' && field.required) {
-            msgRequiresFields.style.display = ''
-            return
-          }
+        // if field other than EMAIL is required, show notice and stop loop
+        if (field.tag !== 'EMAIL' && field.required) {
+          msgRequiresFields.style.display = ''
+          return
         }
       }
-    })
+    }
+  };
+  xhr.send(null);
 }
 
 // init colorpickers
-$context.find('.mc4wp-color').wpColorPicker()
+window.jQuery('.mc4wp-color').wpColorPicker()
 
 // if a list changes, check which fields are required
 elSelectList.addEventListener('change', maybeShowRequiredFieldsNotice)
